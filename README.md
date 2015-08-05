b2ac140e0a71ea65fd9d8d86c50f38bf24cac34d
Cameron McKay <me@cdmckay.org>
2015-08-05 18:08:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Process.st PHP library
+
+You can sign up for a Process.st account at https://process.st.
+
+## Requirements
+
+PHP 5.3.3 and later.
---
+++ lib/ProcessStreet.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ProcessStreet;
+
+class ProcessStreet {
+
+    private static $apiKey;
+
+    private static $apiBase = 'https://api.process.st';
+
+    const VERSION = '0.1.0';
+
+    /**
+     * @return string The API key used in requests.
+     */
+    public static function getApiKey() {
+        return self::$apiKey;
+    }
+
+    /**
+     * @param string $apiKey The API key to be used in requests.
+     */
+    public static function setApiKey($apiKey) {
+        self::$apiKey = $apiKey;
+    }
+
+}
